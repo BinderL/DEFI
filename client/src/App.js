2970b19a1e92b71123fccada666679d3edb0e511
binders <binderl@debian-BULLSEYE-live-builder-AMD64>
2021-11-29 17:49:56 +0100

    fix web3 update, improve client

--- client/src/App.js
+++ client/src/App.js
@@ -9,18 +9,23 @@ import CustomText from "./component/CustomText";
 
 function App() {
 	const [web3, setWeb3] = useState (null);
-	const [accounts, setAccounts] = useState(null);
+	const [accounts, setAccounts] = useState([]);
 	const [contract, setContract] = useState(null)
 	const [voter, setVoter] = useState("");
 	const [voters, setVoters] = useState([]);
 	const [proposal, setProposal] = useState("");
 	const [id_proposal, setId_proposal] = useState(0);
 	const [winner, setWinner] = useState("");	
-				
+			
+	const updateWeb3 = async () => {
+		let accounts = await web3.eth.getAccounts();
+		setAccounts(accounts);
+	}
+
 	const lookForWeb3 = async () => {
 		try {
 			const web3 = await getWeb3();
-			const accounts = await web3.eth.getAccounts();
+			let accounts = await web3.eth.getAccounts();
 			const _networkId = await web3.eth.net.getId();
 			const _deployedNetwork = Voting.networks[_networkId];
 			console.log(accounts);
@@ -40,15 +45,23 @@ function App() {
 	}
 		
 	const run = async () => {
+		console.log("run");
 	
 	}
 
+	lookForWeb3();
+
+	useEffect(() => {
+		if(web3)
+			updateWeb3();
+	});
+
 	const registerVoter = async () => {
-		setVoters((lastVoters) => [...lastVoters, {id:voters.length.toString(),title:voter}]);
-		await contract.methods.addVoter(voter).send({ from: accounts[0] }).then((result) => {
+				await contract.methods.addVoter(voter).send({ from: accounts[0] }).then((result) => {
 						console.log("event: ", result.events.VoterRegistered.event, "was emited and recorded on blockchain");
 						console.log("data recorded: ",result.events.VoterRegistered.returnValues.voterAddress);
 		});
+		setVoters((lastVoters) => [...lastVoters, {id:voters.length.toString(),title:voter}]);
 		setVoter("");
 	}
 	
@@ -111,9 +124,7 @@ function App() {
 
 	}
 			
-	useEffect(() => {
-		lookForWeb3();
-	});
+	lookForWeb3();
 
 	if (!web3) {
 		return <div>Loading Web3, accounts, and contract...... </div>;
@@ -127,6 +138,9 @@ function App() {
 				If your contracts compiled and migrated successfully, below will show
 	the owner of the contract (by default).
 			</p>
+			<CustomText
+				content={"Signer: " + accounts[0]}/>
+
 			<CustomInput 
 				data={voter} 
 				commentaire="Voter Address, message signed by Owner" 
@@ -137,7 +151,8 @@ function App() {
 				onPress={registerVoter} />
 					
 			<CustomStatus
-				data={voters} />
+				data={voters} 
+				action={ () => {console.log("hello");}}/>
 
 			<CustomButton
 				name="Begin Proposal Session"
